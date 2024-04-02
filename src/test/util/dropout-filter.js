844be4952a8dc87323e3af179b39ba2c716a3722
rbarraalmeida <164796559+rbarraalmeida@users.noreply.github.com>
2024-04-02 16:47:23 -0300

    revamp dropout filter.

--- src/test/util/dropout-filter.js
+++ src/test/util/dropout-filter.js
@@ -5,17 +5,35 @@ test('fill spurious zero power measurements with previous value', t => {
   const filter = createDropoutFilter();
   let r;
 
-  r = filter({ power: 100, cadence: 101 });
+  r = filter({ power: 100, cadence: 101, speed: '25.0' });
   t.equal(r.power, 100, 'power (watts)');
   t.equal(r.cadence, 101, 'cadence (rpm)');
+  t.equal(r.speed, '25.0', 'speed (kph)');
 
-  r = filter({ power: 0, cadence: 101 });
+  r = filter({ power: 0, cadence: 101, speed: '0.0' });
   t.equal(r.power, 100, 'power (watts)');
   t.equal(r.cadence, 101, 'cadence (rpm)');
+  t.equal(r.speed, '25.0', 'speed (kph)');
 
-  r = filter({ power: 0, cadence: 101 });
+  r = filter({ power: 0, cadence: 101, speed: '25.0' });
   t.equal(r.power, 0, 'power (watts)');
   t.equal(r.cadence, 101, 'cadence (rpm)');
+  t.equal(r.speed, '25.0', 'speed (kph)');
+
+  r = filter({ power: 100, cadence: 101, speed: '25.0' });
+  t.equal(r.power, 100, 'power (watts)');
+  t.equal(r.cadence, 101, 'cadence (rpm)');
+  t.equal(r.speed, '25.0', 'speed (kph)');
+
+  r = filter({ power: 0, cadence: 0, speed: '25.0' });
+  t.equal(r.power, 100, 'power (watts)');
+  t.equal(r.cadence, 101, 'cadence (rpm)');
+  t.equal(r.speed, '25.0', 'speed (kph)');
+
+  r = filter({ power: 0, cadence: 101, speed: '25.0' });
+  t.equal(r.power, 0, 'power (watts)');
+  t.equal(r.cadence, 101, 'cadence (rpm)');
+  t.equal(r.speed, '25.0', 'speed (kph)');
 
   t.end();
 });
@@ -24,15 +42,15 @@ test('fill spurious zero cadence measurements with previous value', t => {
   const filter = createDropoutFilter();
   let r;
 
-  r = filter({ power: 100, cadence: 101 });
+  r = filter({ power: 100, cadence: 101, speed: '25.0' });
   t.equal(r.power, 100, 'power (watts)');
   t.equal(r.cadence, 101, 'cadence (rpm)');
 
-  r = filter({ power: 100, cadence: 0 });
+  r = filter({ power: 100, cadence: 0, speed: '25.0' });
   t.equal(r.power, 100, 'power (watts)');
   t.equal(r.cadence, 101, 'cadence (rpm)');
 
-  r = filter({ power: 100, cadence: 0 });
+  r = filter({ power: 100, cadence: 0, speed: '25.0' });
   t.equal(r.power, 100, 'power (watts)');
   t.equal(r.cadence, 0, 'cadence (rpm)');
 
