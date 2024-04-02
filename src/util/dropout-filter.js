a29c79d4c941b3fdf5f42bbb482d2129db2378a9
rbarraalmeida <164796559+rbarraalmeida@users.noreply.github.com>
2024-04-02 16:16:49 -0300

    fix dropout filter.

--- src/util/dropout-filter.js
+++ src/util/dropout-filter.js
@@ -15,19 +15,21 @@ export function createDropoutFilter() {
    * @param {object} curr - current stats payload
    * @param {number} curr.power - power (watts)
    * @param {number} curr.cadence - cadence (rpm)
+   * @param {string} curr.speed - speed (km/h)
    * @returns {object} fixed - fixed stats payload
    * @returns {object} fixed.power - fixed power (watts)
    * @returns {object} fixed.cadence - cadence
+   * @returns {string} fixed.speed - speed (km/h)
    */
   return function (curr) {
     let fixed = {...curr};
-    if (prev !== null && curr.power === 0 && (curr.cadence > 0 || curr.speed > 0) && prev.power > 0) {
+    if (prev !== null && curr.power === 0 && (curr.cadence > 0 || parseFloat(curr.speed) > 0) && prev.power > 0) {
       fixed.power = prev.power;
     }
-    if (prev !== null && curr.cadence === 0 && (curr.power > 0 || curr.speed > 0) && prev.cadence > 0) {
+    if (prev !== null && curr.cadence === 0 && (curr.power > 0 || parseFloat(curr.speed) > 0) && prev.cadence > 0) {
       fixed.cadence = prev.cadence;
     }
-    if (prev !== null && curr.speed === 0 && (curr.power > 0 || curr.cadence > 0) && prev.speed > 0) {
+    if (prev !== null && curr.speed === '0.0' && (curr.power > 0 || curr.cadence > 0) && parseFloat(prev.speed) > 0) {
       fixed.speed = prev.speed;
     }
     prev = curr;
