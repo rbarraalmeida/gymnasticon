341de3d8760c231051a71b8adf10869f11d6dd13
rbarraalmeida <164796559+rbarraalmeida@users.noreply.github.com>
2024-04-04 11:44:20 -0300

    Increase bike disconnect timeout

--- src/bikes/keiser.js
+++ src/bikes/keiser.js
@@ -18,7 +18,7 @@ const KEISER_VALUE_IDX_VER_MINOR = 3; // 8-bit Version Major data offset within 
 const KEISER_STATS_NEWVER_MINOR = 30; // Version Minor when broadcast interval was changed from ~ 2 sec to ~ 0.3 sec
 const KEISER_STATS_TIMEOUT_OLD = 7.0; // Old Bike: If no stats received within 7 sec, reset power and cadence to 0
 const KEISER_STATS_TIMEOUT_NEW = 2.0; // New Bike: If no stats received within 2 sec, reset power and cadence to 0
-const KEISER_BIKE_TIMEOUT = 60.0; // Consider bike disconnected if no stats have been received for 60 sec / 1 minutes
+const KEISER_BIKE_TIMEOUT = 30 * 60.0; // Consider bike disconnected if no stats have been received for 30 minutes
 
 const debuglog = require('debug')('gym:bikes:keiser');
 
