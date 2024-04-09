562d1c99d68f2b52013449a237b718025f0884d2
Rodrigo Barra <rbarra@gmail.com>
2024-04-09 23:07:04 +0100

    Revert "First try."
    This reverts commit 265779ab68a5119d0845b6e2c1b8d6b2a173003a.

--- src/app/ui/textClient.js
+++ src/app/ui/textClient.js
@@ -5,6 +5,7 @@ export class TextClient {
    * @param {number} riderFtp - the ftp for the participant.
    */
   constructor(riderFtp) {
+    super();
     this.riderFtp = riderFtp;
   }
 
@@ -15,57 +16,6 @@ export class TextClient {
     this.screen = this.blessed.screen({
       smartCSR: true
     });
-
-    screen.title = 'my window title';
-
-    // Create a box perfectly centered horizontally and vertically.
-    var box = blessed.box(
-        {
-          top: 'center',
-          left: 'center',
-          width: '50%',
-          height: '50%',
-          content: 'Hello {bold}world{/bold}!',
-          tags: true,
-          border: { 
-            type: 'line'
-        },
-        style: {
-            fg: 'white',
-            bg: 'magenta',
-            border: {
-                fg: '#f0f0f0'
-            },
-            hover: {
-                bg: 'green'
-            }
-        }
-    });
-
-    // Append our box to the screen.
-    screen.append(box);
-
-    // If our box is clicked, change the content.
-    box.on('click', function(data) {
-        box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
-        screen.render();
-    });
-
-    // If box is focused, handle `enter`/`return` and give us some more content.
-    box.key('enter', function(ch, key) {
-        box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
-        box.setLine(1, 'bar');
-        box.insertLine(1, 'foo');
-        screen.render();
-    });
-
-    // Quit on Escape, q, or Control-C.
-    screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-        return process.exit(0);
-    });
-
-    // Focus our element.
-    box.focus();
   }
 
   update(cadence, speed, power) {
