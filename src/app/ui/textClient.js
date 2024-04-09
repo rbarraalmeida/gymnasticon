85e211030d46d2cbeb54aa76c5290ef971e1954e
rbarraalmeida <164796559+rbarraalmeida@users.noreply.github.com>
2024-04-09 14:35:40 -0300

    Simpler version.

--- src/app/ui/textClient.js
+++ src/app/ui/textClient.js
@@ -13,10 +13,11 @@ export class TextClient {
     this.blessed = require('blessed');
 
     this.screen = this.blessed.screen({
-      smartCSR: true
+      smartCSR: true,
+      dockBorders: true,
     });
 
-    screen.title = 'my window title';
+    this.screen.title = 'Gymnasticon';
 
     // Create a box perfectly centered horizontally and vertically.
     var box = blessed.box(
@@ -41,31 +42,6 @@ export class TextClient {
             }
         }
     });
-
-    // Append our box to the screen.
-    screen.append(box);
-
-    // If our box is clicked, change the content.
-    box.on('click', function(data) {
-        box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
-        screen.render();
-    });
-
-    // If box is focused, handle `enter`/`return` and give us some more content.
-    box.key('enter', function(ch, key) {
-        box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
-        box.setLine(1, 'bar');
-        box.insertLine(1, 'foo');
-        screen.render();
-    });
-
-    // Quit on Escape, q, or Control-C.
-    screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-        return process.exit(0);
-    });
-
-    // Focus our element.
-    box.focus();
   }
 
   update(cadence, speed, power) {
