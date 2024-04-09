a0d77dfcddcd8c756d8d3564473df3afa0e7fd66
Rodrigo Barra <rbarra@gmail.com>
2024-04-09 23:07:04 +0100

    Revert "Simpler version."
    This reverts commit 85e211030d46d2cbeb54aa76c5290ef971e1954e.

--- src/app/ui/textClient.js
+++ src/app/ui/textClient.js
@@ -13,11 +13,10 @@ export class TextClient {
     this.blessed = require('blessed');
 
     this.screen = this.blessed.screen({
-      smartCSR: true,
-      dockBorders: true,
+      smartCSR: true
     });
 
-    this.screen.title = 'Gymnasticon';
+    screen.title = 'my window title';
 
     // Create a box perfectly centered horizontally and vertically.
     var box = blessed.box(
@@ -42,6 +41,31 @@ export class TextClient {
             }
         }
     });
+
+    // Append our box to the screen.
+    screen.append(box);
+
+    // If our box is clicked, change the content.
+    box.on('click', function(data) {
+        box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
+        screen.render();
+    });
+
+    // If box is focused, handle `enter`/`return` and give us some more content.
+    box.key('enter', function(ch, key) {
+        box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
+        box.setLine(1, 'bar');
+        box.insertLine(1, 'foo');
+        screen.render();
+    });
+
+    // Quit on Escape, q, or Control-C.
+    screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+        return process.exit(0);
+    });
+
+    // Focus our element.
+    box.focus();
   }
 
   update(cadence, speed, power) {
