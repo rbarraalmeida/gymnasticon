4ddb9b2d47db3092af32f4c1110d4a2318ea83a2
rbarraalmeida <164796559+rbarraalmeida@users.noreply.github.com>
2024-04-10 07:19:45 -0300

    hello world.

--- src/datagen/generateData.js
+++ src/datagen/generateData.js
@@ -1,51 +1,17 @@
 #!/usr/bin/env node
 "use strict";
 
-const WHITE_IN_BLACK = { attr: { color: 'white' , bgColor: 'black' },
-                         transparencyChar: '#'};
-const RED_IN_BLACK = { attr: { color: 'red' , bgColor: 'black' },
-                       transparencyChar: '#'};
-const DIR = 'src/datagen/data/'
+const termkit = require('terminal-kit');
+const term = termkit.terminal;
 
-var fs = require('fs');
-var ScreenBuffer = require('terminal-kit').ScreenBuffer;
-var terminal = require('terminal-kit').terminal;
-var term;
 
-var numbers = ['0', '1', '2', '3', 
-    '4', '5', '6', '7', '8', '9', '.'];
 
-var letters = ['A', 'C', 'D', 'E', 'L', 'P', 
-    'R', 'T', 'V'];
+var buffer = new termkit.ScreenBuffer( { dst: term , width: 8 , height: 8 } ) ;
+buffer.fill( { attr: { bgColor: 'cyan' } } ) ;
+buffer.fill( { attr: { bgColor: 'brightMagenta' } , region: { x: 3 , y: 2 , width: 3 , height: 3 } } ) ;
+buffer.draw() ;
 
-//terminal.fullscreen();
-//terminal.hideCursor();
-var screen = new ScreenBuffer( {
-    dst: terminal,
-    width: 8, //Math.min(terminal.width - 2, 40),
-    height: 8, //Math.min(terminal.height -2, 40),
-    //y: 2,
-    //x: 2
-});
-screen.fill({attr:{ color: 'pink' , bgColor: 'cyan'}}); 
-//numbers.forEach((element)=> generateCharacter(element, WHITE_IN_BLACK));
-//letters.forEach((element)=> generateCharacter(element, WHITE_IN_BLACK));
- 
-var xPos = 1;
-var yPos = 1;
+term( '\n' ) ;
 
-function generateCharacter(element, color) {
-  var inputFilename = DIR + element + ".txt";
-  var outputFilename = DIR + element + ".sbuf";
+exit(0);
 
-  var characterSprite = ScreenBuffer.createFromChars(color, fs.readFileSync(inputFilename));
-  characterSprite.saveSync(outputFilename);
-  characterSprite.x = xPos;
-  characterSprite.y = yPos;
-  characterSprite.draw({dst: screen});
-  xPos += 4;
-  if (xPos > 40) {
-    xPos = 1;
-    yPos += 7;
-  }
-}
