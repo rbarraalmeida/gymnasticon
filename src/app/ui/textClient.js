0f7f663523cfa0484803ce45f1849a64a8f6e0bf
rbarraalmeida <164796559+rbarraalmeida@users.noreply.github.com>
2024-04-10 15:45:57 -0300

    Fix package and starts to render the scree.

--- src/app/ui/textClient.js
+++ src/app/ui/textClient.js
@@ -5,6 +5,13 @@ const SPEED_LABEL = "VEL";
 const POWER_LABEL = "POT";
 const POWER_PERC_LABEL = "POR";
 
+const CHARACTERS = ['0', '1', '2', '3', 
+    '4', '5', '6', '7', '8', '9', '.',
+    'A', 'C', 'D', 'E', 'L', 'P', 
+    'R', 'T', 'V'];
+
+const termkit = require('terminal-kit');
+
 export class TextClient {
   /**
    * Create a TextClient instance.
@@ -15,12 +22,29 @@ export class TextClient {
     this.cadence = 0;
     this.speed = 0;
     this.power = 0;
+    this.sprites = {};
+    this.yPos = 1;
+    this.xPos = 1;
   }
 
   build() {
-    this.term = require('terminal-kit').terminal;
-    
+    this.term = termkit.terminal;
+    this.term.fullscreen();
+    this.term.hideCursor();
+
+    this.buffer = new termkit.ScreenBuffer( 
+      { dst: this.term , 
+        width: Math.max(this.term.width - 1, 40), 
+        height: Math.max(this.term.height -1, 40)}) ;
+
+    CHARACTERS.forEach((element) => loadSprite(element));
+
     this.draw();
+  }
+
+  loadSprite(character) {
+    this.sprites[character] = termkit.ScreenBuffer.loadSync(
+      `${SPRITE_DIR}${character}.sbuf`);
   }
 
   /**
@@ -46,12 +70,18 @@ export class TextClient {
     lines.push(`${POWER_LABEL}    ${this.power}`);
     lines.push(`${POWER_PERC_LABEL}    ${power_perc.toFixed(0)}`);
 
-    console.log("\n\n\n\nStarting to draw");
-    lines.forEach((element) => console.log(`${element}`));
-
+    this.buffer.fill({ attr: { bgColor: 'black' }});
+    this.yPos = 1;
+    lines.forEach((element) => drawLine(element));
+    
+    this.buffer.draw();
     // adds a callback to draw things
     var _this = this;
     setTimeout(function() { _this.draw(); }, 50);
+  }
+
+  drawLine(line) {
+    this.xPos = 1;
   }
 }
 
