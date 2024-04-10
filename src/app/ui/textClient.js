006c696dabb5eb369001e0003a1b8e90ce391432
rbarraalmeida <164796559+rbarraalmeida@users.noreply.github.com>
2024-04-10 16:45:54 -0300

    Adds O.

--- src/app/ui/textClient.js
+++ src/app/ui/textClient.js
@@ -8,7 +8,7 @@ const SPRITE_DIR = "/home/pi/gymnasticon/lib/datagen/data/";
 const CHARACTERS = ['0', '1', '2', '3', 
     '4', '5', '6', '7', '8', '9', '.',
     'A', 'C', 'D', 'E', 'L', 'P', 
-    'R', 'T', 'V'];
+    'R', 'T', 'V', 'O'];
 
 const termkit = require('terminal-kit');
 
--- src/datagen/generateData.js
+++ src/datagen/generateData.js
@@ -19,7 +19,7 @@ const numbers = ['0', '1', '2', '3',
     '4', '5', '6', '7', '8', '9', '.'];
 
 const letters = ['A', 'C', 'D', 'E', 'L', 'P', 
-    'R', 'T', 'V'];
+    'R', 'T', 'V', 'O'];
 
 term.fullscreen();
 term.hideCursor();
