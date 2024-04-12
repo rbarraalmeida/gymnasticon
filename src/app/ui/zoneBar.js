de8ceb888acce1031b4ea007478c2ef7fe332eff
rbarraalmeida <164796559+rbarraalmeida@users.noreply.github.com>
2024-04-12 10:23:20 -0300

    y

--- src/app/ui/zoneBar.js
+++ src/app/ui/zoneBar.js
@@ -94,7 +94,7 @@ export class ZoneBar {
                       height: CUR_ZONE_HEIGHT } } ) ;
         //console.log(`\t\t\tlight x:${xPos + finalDarkPos} y:0 width: ${spaceForCurrentZone - finalDarkPos} height: ${CUR_ZONE_HEIGHT}`);
         var label = `Zone ${zoneToDraw.id}: ${zoneToDraw.name}`;
-        var charPos = xPos + finalDarkPos; // - Math.floor(label.lenght/2);
+        var charPos = xPos + Math.floor(spaceForCurrentZone/2); // - Math.floor(label.lenght/2);
         buffer.moveTo(charPos, 1);
         label.split('').forEach((character) => {
           var charAttr;
