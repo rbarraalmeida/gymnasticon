349c47ce53e9399780945ba29713f5710f70ecef
rbarraalmeida <164796559+rbarraalmeida@users.noreply.github.com>
2024-05-20 10:55:50 -0300

    increases the timeout to 3 seconds.

--- src/bikes/keiser.js
+++ src/bikes/keiser.js
@@ -16,8 +16,8 @@ const KEISER_VALUE_IDX_REALTIME = 4; // Indicates whether the data present is re
 const KEISER_VALUE_IDX_VER_MAJOR = 2; // 8-bit Version Major data offset within packet
 const KEISER_VALUE_IDX_VER_MINOR = 3; // 8-bit Version Major data offset within packet
 const KEISER_STATS_NEWVER_MINOR = 30; // Version Minor when broadcast interval was changed from ~ 2 sec to ~ 0.3 sec
-const KEISER_STATS_TIMEOUT_OLD = 7.0; // Old Bike: If no stats received within 7 sec, reset power and cadence to 0
-const KEISER_STATS_TIMEOUT_NEW = 2.0; // New Bike: If no stats received within 2 sec, reset power and cadence to 0
+const KEISER_STATS_TIMEOUT_OLD = 7.0; // Old Bike: If no stats received within 7 sec
+const KEISER_STATS_TIMEOUT_NEW = 3.0; // New Bike: If no stats received within 3 sec
 const KEISER_BIKE_TIMEOUT = 10 * 60.0; // Consider bike disconnected if no stats have been received for 20 minutes
 
 const debuglog = require('debug')('gym:bikes:keiser');
